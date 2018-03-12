9430ab9838d1bd12cb468bd3323c81a48672fbc8
linjunjie <linjunjie@qiyi.com>
2018-03-12 18:47:13 +0800

    implement the BlockChain class

---
+++ blockchain.php
@@ -0,0 +1,142 @@
+<?php
+
+class BlockChain{
+
+	protected $current_transaction = [];
+	protected $chain = [];
+	protected $nodes = [];
+
+	public function __construct(){
+		$this->current_transaction = [];
+		$this->chain = [];
+		$this->nodes = [];
+
+		$this->new_block($proof = 100, $previous_hash='1');
+	}
+
+	public function register_node($address){
+		$parsed_url = parse_url($address);
+
+		if (is_array($parse_url)) {
+			$this->nodes[] = $parsed_url['host'] . ':' . $parsed_url['port'];
+		} else {
+			return false;
+		}
+	}
+
+	public static function valid_chain($chain){
+		
+		$last_block = $chain[0];
+		$current_index = 1;
+		while ($current_index < count($chain)) {
+			$block = $chain[$current_index];
+			if($block[$previous_hash] != self::hash($last_block)){
+				return false;
+			}
+
+			if(!$this->valid_proof($last_block['proof'], $block['proof'], $last_block['previous_hash'])){
+				return false;
+			}
+
+			$last_block = $block;
+			$current_index += 1;
+		}
+
+		return true;
+	}
+
+	public function valid_proof($last_proof, $proof, $last_hash){
+		$guess = $last_proof . $proof . $last_hash;
+		$guess_hash = hash('sha256', $guess);
+		return substr($guess_hash, 0, 4) == '0000';
+	}
+
+	public function resolve_conflicts(){
+		$neighbour = $this->nodes;
+		$new_chain = null;
+
+		$max_length = count($this->chain);
+
+		foreach ($node as $neighbour) {
+			$response = $this->request("http://{$node}/chain");
+			if($response->status_code == 200){
+				$length = $response->length;
+				$chain = $response->chain;
+
+				if($length > $max_length && self::valid_chain($chain)){
+					$max_length = $length;
+					$new_chain = $chain;
+				}
+			}
+		}
+
+		if(!is_null($new_chain)){
+			$this->chain = $new_chain;
+			return true;
+		}
+
+		return false;
+	}
+
+	public function new_block($proof, $previous_hash){
+		$block = [
+			'index' => count($this->chain) + 1,
+			'timestamp' => time(),
+			'transaction' => $this->current_transaction,
+			'proof' => $proof,
+			'previous_hash' => $previous_hash ?: self::hash(end($this->chain))
+		];
+
+		$this->current_transaction = [];
+		$this->chain[] = $block;
+
+		return $block;
+	}
+
+	public function new_transaction($sender, $recipient, $amount){
+		$this->current_transaction[] = [
+			'sender' => $sender,
+			'recipient' => $recipient,
+			'amount' => $amount,
+		];
+
+		$last_block = $this->last_block();
+		return $last_block['index'] + 1;
+	}
+
+	public function last_block(){
+		return end($this->chain);
+	}
+
+	public function hash($block){
+		$block_string = json_encode($block);
+		return hash('sha256', $block_string);
+	}
+
+	public function proof_of_work($last_block){
+		$last_proof = $last_block['proof'];
+		$last_hash = self::hash($last_block);
+		$proof = 0;
+		while ($this->valid_proof($last_proof, $proof, $last_hash) == false) {
+			$proof += 1;
+		}
+
+		return $proof;
+	}
+
+	public function uuid(){
+		$uuid = $this->gen_uuid();
+		return str_replace('-', '', $uuid);
+	}
+
+	private function gen_uuid(){
+		return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+			mt_rand(0, 0xffff), mt_rand(0, 0xffff),
+			mt_rand(0, 0xffff),
+			mt_rand(0, 0x0fff)| 0x4000,
+			mt_rand(0, 0x3fff)| 0x8000,
+			mt_rand(0, 0xffff), mt_rand(0, 0xffff),mt_rand(0, 0xffff)
+		);
+	}
+
+}
